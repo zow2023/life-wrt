d3ae4e965806483513342b683e71cf946bdb1a24
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-05-17 17:52:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,18 +20,18 @@
 #git clone https://github.com/pymumu/luci-app-smartdns.git -b lede feeds/luci/applications/luci-app-smartdns/
 #git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
 #git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/chinadns-ng
-rm -rf feeds/luci/applications/luci-app-passwall
+#rm -rf feeds/packages/net/xray-core
+#rm -rf feeds/packages/net/chinadns-ng
+#rm -rf feeds/luci/applications/luci-app-passwall
 
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b  luci-smartdns-dev package/passwall/luci
-rm -rf feeds/package/helloworld
-git clone https://github.com/fw876/helloworld.git -b master package/helloworld
+#rm -rf feeds/package/helloworld
+#git clone https://github.com/fw876/helloworld.git -b master package/helloworld
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 #find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 #find ./ | grep Makefile | grep mosdns | xargs rm -f
